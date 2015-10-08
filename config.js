8bebbf9f6af00909b0b4a2bc9fb1a087747e12db
SnapDev <siul1213@gmail.com>
2015-10-08 15:04:23 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "GJC Calendar powered by SnapDev",
+   "version": "2.3",
+   "homepage": "https://a3259ac9d029125cd976893372c7c929b27ecb91.googledrive.com/host/0B5zmXTs4bwSPYmF4c09ON0FFNGM/",
+   "enableLogoutBttn": false,
+   "termsOfService": "Free to Use powered by SnapDev \u000amaintained by HRD Office\u000aGeneral de Jesus College for Chrome Apps",
+   "kioskEnabled": true
+};
